2ef7f3325a38b3f970336ad91add112b53d75177
KeysStarks <keishawnstarks@outlook.com>
2026-04-08 10:00:37 -0400

    Final Portfolio Project

---
+++ script.js
@@ -0,0 +1,12 @@
+const toggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+toggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
+});
